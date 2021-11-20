b12a52e2ccee60c3d9ae899d41b6a44f84e53aee
bilbo <axel.catois@gmail.com>
2021-11-20 16:57:29 +0100

    Update to handle searching for client

--- roads/characters.js
+++ roads/characters.js
@@ -4,7 +4,12 @@ const router = express.Router();
 
 router.get("/characters", async (req, res) => {
   try {
-    const response = await axios.get(`https://lereacteur-marvel-api.herokuapp.com/characters?apiKey=${process.env.MARVEL_API_KEY}&limit=${req.query.limit}`);
+    let url = `https://lereacteur-marvel-api.herokuapp.com/characters?apiKey=${process.env.MARVEL_API_KEY}&limit=${req.query.limit}&skip=${(req.query.page - 1) * req.query.limit}`;
+    if (req.query.title) {
+      url = url + `&name=${req.query.title}`;
+    }
+    console.log(url);
+    const response = await axios.get(url);
 
     res.json(response.data);
   } catch (error) {
--- roads/comics.js
+++ roads/comics.js
@@ -4,7 +4,11 @@ const router = express.Router();
 
 router.get("/comics", async (req, res) => {
   try {
-    const response = await axios.get(`https://lereacteur-marvel-api.herokuapp.com/comics?apiKey=${process.env.MARVEL_API_KEY}&limit=${req.query.limit}&skip=${(req.query.page - 1) * req.query.limit}`);
+    let url = `https://lereacteur-marvel-api.herokuapp.com/comics?apiKey=${process.env.MARVEL_API_KEY}&limit=${req.query.limit}&skip=${(req.query.page - 1) * req.query.limit}`;
+    if (req.query.title) {
+      url = url + `&title=${req.query.title}`;
+    }
+    const response = await axios.get(url);
 
     res.json(response.data);
   } catch (error) {
